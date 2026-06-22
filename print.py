70118a8b99b3cf6dc019fda9cf939ec022e76b45
reyhana27 <reyhanasrs27@gmail.com>
2026-06-22 08:39:28 -0400

    Enhance print.py with examples and tasks
    Added tasks and examples for using print() in Python.

--- print.py
+++ print.py
@@ -3,3 +3,28 @@
 print("Hello") # will display "Hello" as an output
 
 # Task: write a python code to print "Mango"
+
+# You can separate numbers and strings in print() by using a ","
+
+print("There are ", 4, " cats" ) # will display "There are 4 cats". The strings and the number are separated
+
+# Task: write a python code to print "I have 4 marbles"
+
+# When printing numbers, quotes aren't needed.
+
+print(300)
+print(2)
+
+# You can do math in print()
+
+print(2 + 1)
+print(3 * 3)
+
+# Task: print 5 - 3 as an output
+
+# if you use "end=" inside print(), you can connect two separate print() lines
+
+print("Hi there", end=" ")
+print("How much is this bag?") # will output "Hi there How much is this bag?"
+
+# Task: using print() on separate lines, connect two words. the 2 words are "apple" and "sauce"
