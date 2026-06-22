6c217ef5c0ca0d75da35ce9a4b2fc3d6de649cdc
reyhana27 <reyhanasrs27@gmail.com>
2026-06-22 08:57:10 -0400

    Create variables.py with examples of variable types
    Added examples of variable types and usage in Python.

---
+++ variables.py
@@ -0,0 +1,55 @@
+# You can store values in variables
+
+# There are 4 types of variables in Python: Integers, Floats, Booleans and Strings
+
+# An integer is a numerical value that does not contain any decimal points
+
+NumOfAnimals = 9
+
+# A float is a numerical value that does contain decimal points
+
+Number = 1.233
+
+# A Boolean is True or False
+
+GoodWheather = False
+BadWheather = True
+
+# A string is an alphabetical value and is typed in quotes
+
+Name = "Emily"
+
+# To create a variable, you should name it something meaningful and set it equal to a value
+
+NumberOfEggs = 60  # the variable names "NumberOfEggs" contains the value 60
+
+# you can print variables by using print()
+
+print(Name) # will print "Emily" as an output
+
+# You can specify your variable types!
+
+x = str(6)
+n = int(6)
+b = float(6)
+v = bool(6)
+
+# you can get the type of your variable
+
+print(type(x)) # will output "string"
+
+# Variable names are case sensitive
+
+A = 8
+a = 0
+# A and a are both considered different variables
+
+# You can assign values to multiple variables
+
+a, b, c = "Hi", "Bye", "Why"
+
+# You can assign same value to multiple variables in a signle line
+
+q = r = s = "Helloo"
+
+# Task: create 4 different variables and specify their types and then print the variable values and their types
