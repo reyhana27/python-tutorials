796455d64a2d9f9feabf1ab6f9c32511456ee2d0
reyhana27 <reyhanasrs27@gmail.com>
2026-06-22 09:05:14 -0400

    Implement basic type casting examples in casting.py
    Added examples of type casting in Python.

---
+++ casting.py
@@ -0,0 +1,13 @@
+# Casting in Python can help you specify you variable's type
+
+# int() will remove the decimal portion of the number in parenthesis
+
+# str() will set anything put in parenthesis to a string even if it is a number
+
+# float() will set anything in parenthesis to a number that contains decimals
+
+g = int(3.4) # value becomes 3
+v = str(5) # 5 becomes a string
+b = float(2) # 2 becomes a float instead of an integer
+
+# task: varible r is "3" (string 3) but you specify it to be an integer value
