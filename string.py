f0ef918373a59a454978b84ed87eb3ba0ade5297
reyhana27 <reyhanasrs27@gmail.com>
2026-07-24 11:07:14 +0400

    Create string.py

---
+++ string.py
@@ -0,0 +1,16 @@
+# Strings in Python are used to store text
+
+# A string must be placed inside quotation marks
+
+# You can use either single quotes or double quotes
+
+name = "lily"          # name stores a string
+language = 'Python'    # language also stores a string
+
+message = "Hello " + name   # joins two strings together
+print(message)
+
+print(len(language))        # displays the number of characters
+
+# task: create a variable called city and set it to your city's name
+# then print the sentence "I live in city"
