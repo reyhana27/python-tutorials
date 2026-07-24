06f0189d32ae40764776a8ba52108538d527a16d
reyhana27 <reyhanasrs27@gmail.com>
2026-07-24 11:08:01 +0400

    Create operator.py

---
+++ operator.py
@@ -0,0 +1,24 @@
+# Operators in Python are used to perform calculations and comparisons
+
+# + adds two values
+# - subtracts one value from another
+# * multiplies two values
+# / divides one value by another
+
+a = 10
+b = 5
+
+addition = a + b        # result is 15
+subtraction = a - b     # result is 5
+multiplication = a * b  # result is 50
+division = a / b        # result is 2.0
+
+print(addition)
+print(subtraction)
+print(multiplication)
+print(division)
+
+# == checks if two values are equal
+print(a == b)           # result is False
+
+# task: create two variables and display their sum, difference and product
