eaf15ae91f7e23b4bdb1aad2729b8d72de9315c4
reyhana27 <reyhanasrs27@gmail.com>
2026-07-24 11:09:28 +0400

    Create while.py

---
+++ while.py
@@ -0,0 +1,20 @@
+# A while loop repeats code while a condition is True
+
+number = 1
+
+while number <= 5:
+    print(number)
+    number = number + 1
+
+# The variable must change inside the loop
+# otherwise the loop may continue forever
+
+countdown = 3
+
+while countdown > 0:
+    print(countdown)
+    countdown = countdown - 1
+
+print("Finished")
+
+# task: use a while loop to print the numbers from 10 down to 1
