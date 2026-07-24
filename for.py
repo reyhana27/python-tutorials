5ca9eb1c39ec5b306770ffa589c444fe556de4d8
reyhana27 <reyhanasrs27@gmail.com>
2026-07-24 11:09:12 +0400

    Add examples of for loops in Python
    Demonstrate usage of for loops with range and lists.

---
+++ for.py
@@ -0,0 +1,20 @@
+# A for loop repeats code for every item in a sequence
+
+# range(5) produces the numbers 0, 1, 2, 3 and 4
+
+for number in range(5):
+    print(number)
+
+# A for loop can also go through items in a list
+
+fruits = ["apple", "banana", "orange"]
+
+for fruit in fruits:
+    print(fruit)
+
+# range(1, 6) produces the numbers 1 through 5
+
+for number in range(1, 6):
+    print(number)
+
+# task: use a for loop to print the numbers from 1 to 10
